ad9b7b45c770e08816044169cf4a2ff0ffe5dd1c
Adryelgcouto <adryelgcouto@gmail.com>
2023-04-03 15:08:33 -0300

    adicionado a função do botão aparecer

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //linkar o js com o html
+let nomeFood = "";
+let nomeDrink = "";
+let nomeDessert = "";
+function verficarPedidos(){
+  if(nomeFood !== ""){
+    if(nomeDrink !== ""){
+      if(nomeDessert !==""){
+        const btn = document.querySelector(".confirm")
+        const btnDisable = document.querySelector(".realizing")
+        btnDisable.classList.add("disable")
+        btn.classList.remove("disable")
+
+      }
+    }
+  }
+}
+
 function selecionadoFood(seletor) {
+  nomeFood = seletor.innerHTML;
   console.log(seletor)
   const btnSelecionado = document.querySelector('.food .selecionado')
   if (btnSelecionado !== null) {
@@ -8,11 +26,13 @@ function selecionadoFood(seletor) {
   //marcar o prato
   const bntClicado = document.querySelector(seletor)
   bntClicado.classList.add('selecionado')
+  verficarPedidos()
 }
 
 //fazer com que quando clicar em outro prato deselecionar o outro prato
 //Drink
 function selecionadoDrink(seletor) {
+  nomeDrink = seletor.innerHTML;
   console.log(seletor)
   const btnSelecionado = document.querySelector('.drink .selecionado')
   if (btnSelecionado !== null) {
@@ -21,9 +41,11 @@ function selecionadoDrink(seletor) {
   //marcar o prato
   const bntClicado = document.querySelector(seletor)
   bntClicado.classList.add('selecionado')
+  verficarPedidos()
 }
 //dessert
 function selecionadoDessert(seletor) {
+  nomeDessert = seletor.innerHTML;
   console.log(seletor)
   const btnSelecionado = document.querySelector('.dessert .selecionado')
   if (btnSelecionado !== null) {
@@ -32,4 +54,5 @@ function selecionadoDessert(seletor) {
   //marcar o prato
   const bntClicado = document.querySelector(seletor)
   bntClicado.classList.add('selecionado')
+  verficarPedidos()
 }
