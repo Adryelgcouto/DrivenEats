2a7796b6ee22840a1280bb0a6f6928c15bad5dfe
Adryelgcouto <adryelgcouto@gmail.com>
2023-04-03 14:33:04 -0300

    opção de selecionar adicionado

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-//linkar o js com o html 
-function selecionadoFood(seletor){
+//linkar o js com o html
+function selecionadoFood(seletor) {
   console.log(seletor)
-  const prato1 = document.querySelector(".selecionado")
-  //marcar o prato 
-  prato1.classList.remove("selecionado");
+  const btnSelecionado = document.querySelector('.food .selecionado')
+  if (btnSelecionado !== null) {
+    btnSelecionado.classList.remove('selecionado')
+  }
+  //marcar o prato
   const bntClicado = document.querySelector(seletor)
-  bntClicado.classList.add("selecionado")
+  bntClicado.classList.add('selecionado')
 }
- 
+
 //fazer com que quando clicar em outro prato deselecionar o outro prato
+//Drink
+function selecionadoDrink(seletor) {
+  console.log(seletor)
+  const btnSelecionado = document.querySelector('.drink .selecionado')
+  if (btnSelecionado !== null) {
+    btnSelecionado.classList.remove('selecionado')
+  }
+  //marcar o prato
+  const bntClicado = document.querySelector(seletor)
+  bntClicado.classList.add('selecionado')
+}
+//dessert
+function selecionadoDessert(seletor) {
+  console.log(seletor)
+  const btnSelecionado = document.querySelector('.dessert .selecionado')
+  if (btnSelecionado !== null) {
+    btnSelecionado.classList.remove('selecionado')
+  }
+  //marcar o prato
+  const bntClicado = document.querySelector(seletor)
+  bntClicado.classList.add('selecionado')
+}
